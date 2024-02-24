86cb796aa4e0fb0e76574ae2c91260ae34cff957
Mhmtshn6 <mehmet68064@gmail.com>
2024-02-24 23:22:15 +0300

    Basic Encapsulation Example
    Simple example of Encapsulation , one of the basic principles of oop

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data d1 = new Data();
+            d1.Name = "Admin";
+            d1.Age = 30;
+        }
+    }
+
+    class Data
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
+
